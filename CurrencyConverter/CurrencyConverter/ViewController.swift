32e0f4183489c02123634e18adaeabaaa4f42c03
Jordan Davis <jordan.m.davis92@gmail.com>
2019-06-03 19:36:35 -0400

    conversion logic
    method to calculate currency based on the country

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    enum CurrencyType: Double {
+        case cad = 1.34
+        case peso = 19.77
+    }
+    
+    //MARK: - Functions
+    
+    func convert(dollars: Double, to unit: CurrencyType) -> Double {
+        guard let dollarString = fromCurrencyTextField.text,
+            let dollars = Double(dollarString) else {
+            return 0.0
+        }
+        
+        switch unit {
+        case .peso:
+            _ = dollars * currencyTypes.peso.rawValue
+        case .cad:
+            _ = dollars * currencyTypes.cad.rawValue
+        }
+        
+        return 0.0
+    }
+        
+        
+            
+    
+    
 
+
+    //MARK: - Actions
+    
+    @IBAction func convert(_ sender: Any) {
+        
+    
+    }
+    
+    @IBAction func cadConvert(_ sender: Any) {
+        toCurrencyLabel?.text = "Currency (CAD)"
+    }
+    
+    @IBAction func pesoConvert(_ sender: Any) {
+        toCurrencyLabel?.text = "Currency (Peso)"
+    }
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var pesoButton: UIButton!
+    
+    var currencyTypes = CurrencyType.self
 }
 
