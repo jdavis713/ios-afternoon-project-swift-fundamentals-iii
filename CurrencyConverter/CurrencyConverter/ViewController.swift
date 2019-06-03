1de0aff2cff1399a46fb127a43ee65c6dfbcb60d
Jordan Davis <jordan.m.davis92@gmail.com>
2019-06-03 15:24:29 -0400

    initial commit
    XCode project start

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Jordan Davis on 6/3/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
