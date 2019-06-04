38a4493a7b6f869562e06661c5afcf6ee5233f0a
Jordan Davis <jordan.m.davis92@gmail.com>
2019-06-04 11:57:46 -0400

    final commit
    view controller completed and stretch goal  accomplished

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -23,42 +23,54 @@ class ViewController: UIViewController {
     //MARK: - Functions
     
     func convert(dollars: Double, to unit: CurrencyType) -> Double {
-        guard let dollarString = fromCurrencyTextField.text,
-            let dollars = Double(dollarString) else {
-            return 0.0
+        if unit == CurrencyType.cad {
+            conversionResult = dollars * cadConversion
+        } else if unit == CurrencyType.peso {
+            conversionResult = dollars * pesoConversion
         }
-        
-        switch unit {
-        case .peso:
-            _ = dollars * currencyTypes.peso.rawValue
-        case .cad:
-            _ = dollars * currencyTypes.cad.rawValue
-        }
-        
-        return 0.0
+        return conversionResult
     }
         
     
     //MARK: - Actions
     
     @IBAction func convert(_ sender: Any) {
-        if cadButton.isSelected == true,
-           let dollarString = fromCurrencyTextField.text,
-            let dollars = Double(dollarString) {
-            let canConversion = convert(dollars: dollars, to: .cad)
-        } else if pesoButton.isSelected == true,
-            let dollarString = fromCurrencyTextField.text,
-            let dollars = Double(dollarString) {
-            let pesoConversion = convert(dollars: dollars, to: .peso)
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.numberStyle = .currency
+        
+        guard let userInput = fromCurrencyTextField.text,
+        let userInputDouble = Double(userInput)
+        else { return }
+        
+        if toCurrencyLabel.text == "Currency (CAD)" {
+            
+            let number: NSNumber = NSNumber(value: convert(dollars: userInputDouble, to: .cad))
+            let formattedNumber = currencyFormatter.string(from: number)
+            if let unWrappedNumber = formattedNumber {
+                toCurrencyTextField.text = unWrappedNumber
+            }
+            
+        } else if toCurrencyLabel.text == "Currency (Peso)" {
+            
+            let number: NSNumber = NSNumber(value: convert(dollars: userInputDouble, to: .peso))
+            let formattedNumber = currencyFormatter.string(from: number)
+            if let unwrappedNumber = formattedNumber {
+                toCurrencyTextField.text = unwrappedNumber
+            }
+            
         }
     }
     
     @IBAction func cadConvert(_ sender: Any) {
+        currencyType = CurrencyType.cad
         toCurrencyLabel?.text = "Currency (CAD)"
+        toCurrencyTextField.text = "$"
     }
     
     @IBAction func pesoConvert(_ sender: Any) {
+        currencyType = CurrencyType.peso
         toCurrencyLabel?.text = "Currency (Peso)"
+        toCurrencyTextField.text = "$"
     }
     
     //MARK: - Properties
@@ -69,6 +81,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var pesoButton: UIButton!
     
-    var currencyTypes = CurrencyType.self
+    var currencyType = CurrencyType.cad
+    let cadConversion: Double = 1.34
+    let pesoConversion: Double = 19.77
+    var conversionResult: Double = 0.0
 }
 
+
+//if cadButton.isSelected == true,
+//    let dollarString = fromCurrencyTextField.text,
+//    let dollars = Double(dollarString) {
+//    let canConversion = convert(dollars: dollars, to: .cad)
+//} else if pesoButton.isSelected == true,
+//    let dollarString = fromCurrencyTextField.text,
+//    let dollars = Double(dollarString) {
+//    let pesoConversion = convert(dollars: dollars, to: .peso)
+//}
+
+
+//guard let dollarString = fromCurrencyTextField.text,
+//    let dollars = Double(dollarString) else {
+//        return 0.0
+//}
+//
+//switch unit {
+//case .peso:
+//    _ = dollars * CurrencyType.peso.rawValue
+//case .cad:
+//    _ = dollars * CurrencyType.cad.rawValue
+//}
+//
+//return 0.0
